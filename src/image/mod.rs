2ccea033b1d2f6669dbe32095e041b2e0718ed1d
Masato Hori <rillomas@gmail.com>
2015-06-15 16:23:17 +0900

    Vec<u16> can now be successfully output

--- src/image/mod.rs
+++ src/image/mod.rs
@@ -1,10 +1,16 @@
-#[derive(Debug, Copy, Clone)]
+extern crate std;
+use std::io::prelude::*;
+use std::io::BufWriter;
+use std::fs::File;
+use std::mem;
+
+#[derive(Debug, Copy, Clone, PartialEq)]
 pub enum ImageFormat {
 	GrayScale,
 	Rgb32,
 }
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, PartialEq)]
 pub struct ImageHeader {
 	pub width: usize,
 	pub height: usize,
@@ -15,6 +21,7 @@ pub struct Image {
 	pub header: ImageHeader,
 	pub data: Vec<u16>,
 }
+
 impl Image {
 	pub fn new(width: usize, height: usize, format: ImageFormat) -> Image {
 		Image {
@@ -23,8 +30,24 @@ impl Image {
 				height: height,
 				format: format,
 			},
-			data: Vec::new(),
+			data: vec![0x1234; width*height],
 		}
+	}
+
+	/// Write raw image content to the specified path
+	pub fn write_to_file(&self, path: &str) -> std::io::Result<()> {
+		let f = try!(File::create(path));
+		let mut bw = BufWriter::new(f);
+		// println!("before length: {}", self.data.len());
+		let sl: &[u8];
+		unsafe {
+			sl = std::slice::from_raw_parts(
+				self.data.as_ptr() as *const u8,
+				self.data.len() * mem::size_of::<u16>());
+		}
+		// println!("after length: {}", sl.len());
+		try!(bw.write_all(sl));
+		return Ok(())
 	}
 
 }
--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,38 @@ use rand::Rng;
 mod image;
 
 fn set_random_data(img: &mut image::Image) {
-	let size = img.header.width*img.header.height;
 	let mut rng = rand::thread_rng();
 	let ref mut data = img.data;
-	data.clear();
-	data.reserve(size);
-	for _ in 0..size {
-		data.push(rng.gen::<u16>());
+	for i in 0..data.len() {
+		data[i] = rng.gen::<u16>();
+	}
+}
+
+fn average_filter(input: &image::Image, kernel_size: usize, output: &mut image::Image) {
+	assert!(input.header == output.header);
+	let hdr = input.header;
+	let xl = hdr.width / kernel_size;
+	let yl = hdr.height / kernel_size;
+	for y in 0..yl {
+		let yidx = y *kernel_size;
+		for x in 0..xl {
+			let xidx = x * kernel_size;
+			let mut sum : u32 = 0;
+			for ky in 0..kernel_size {
+				let row_start = (yidx+ky) * hdr.width;
+				for kx in 0..kernel_size {
+					let idx = row_start + (xidx + kx);
+					sum += input.data[idx] as u32;
+				}
+			}
+			let avg = (sum / ((kernel_size*kernel_size) as u32)) as u16;
+			let top_idx = yidx*hdr.width + xidx;
+			output.data[top_idx] = avg;
+			output.data[top_idx + 1] = avg;
+			let btm_idx = (yidx + 1)*hdr.width + xidx;
+			output.data[btm_idx] = avg;
+			output.data[btm_idx + 1] = avg;
+		}
 	}
 }
 
@@ -22,4 +47,12 @@ fn main() {
 	let msg = format!("img: {:4}x{:4} {:?}", hdr.width, hdr.height, hdr.format);
 	println!("{}", msg);
 	set_random_data(&mut img);
+
+	img.write_to_file("before.bin").unwrap();
+
+	// apply average filter
+	let mut tmp = image::Image::new(w, h, image::ImageFormat::GrayScale);
+	average_filter(&img, 2, &mut tmp);
+
+	tmp.write_to_file("after.bin").unwrap();
 }
